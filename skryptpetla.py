dc6431e758db7f63489999a799f235ed525f37cf
Przemyslaw Kuc <przemek903@gazeta.pl>
2014-03-31 19:37:48 +0200

    Change to frame looping, add nodel.cae and modelKomory.odb(20 frames)

--- skryptpetla.py
+++ skryptpetla.py
@@ -20,26 +20,24 @@ odb = session.openOdb('D:\WorkstationAbqus\modelKomory.odb')
 
 #Liczba frame w danym step'ie
 #Wartosc step['Obciazenie'] nalezy ustawic zgodnie z nazwa badanego step'u
-#numberFrames = len(odb.steps['Obciazenie'].frames)
+numberFrames = len(odb.steps['Step-1'].frames)
 
-#kolejne step'y
-nextSteps = ['Step-1', 'Step-2', 'Step-3' , 'Step-4', 'Step-5', 'Step-6', 'Step-7',\
-             'Step-8','Step-9', 'Step-10', 'Step-11', 'Step-12', 'Step-13', 'Step-14',\
-             'Step-15', 'Step-16', 'Step-17' ]
+
 
 #Plik file jest plikiem w ktorym zapisywane sa wyniki 
 file = open("D:\magisterka\Python Abaqus praca\model.txt", 'w+b')
 
-for st in nextSteps:
+for num in range(numberFrames):
         #Ostatni frame w danym step'ie
-        lastFrame = odb.steps[st].frames[-1]
+        currentFrame = odb.steps['Step-1'].frames[num]
         
         #Zmienna displacement przechowuje wartosc przemieszczen 'U' w ostatnim frame'ie\
         # w badanym step'ie
         # Zmienna displacement przechowuje wartosc naprezen 'S'
         
-        displacement = lastFrame.fieldOutputs['U']
-        stress = lastFrame.fieldOutputs['S']
+        displacement = currentFrame.fieldOutputs['U']
+        stress = currentFrame.fieldOutputs['S']
+        strain = currentFrame.fieldOutputs['LE']
         
         #Zmienna nodeS przechowuje referencje do wybranego nodeSets( zadanego set'u), 
         #instance odnosi sie wybranej czesci
@@ -56,17 +54,23 @@ for st in nextSteps:
         centerDisplacement = displacement.getSubset(region=nodeS)
         centerStress = stress.getSubset(region=elementS)
         
-        file.write( "\nStep: %s" % st + '\n' )
+        file.write( "\nFrame: %s" % num + '\n' )
         
         #petla wyswietlajaca wartosc przemieszczen w wezlach( wartosci w
         #jednostkach wymiarowych
         for v in centerDisplacement.values:
             file.write( "Node nr. %s   X: %s   Y: %s   Z: %s \n" % (v.nodeLabel, v.data[0], v.data[1], v.data[2]) + '\n' )
         
-        #petla wyswietlajaca wartosc naprezen w wezlach( wartosci w
+        #petla wyswietlajaca wartosc naprezen w Elementach( wartosci w
         #jednostkach wymiarowych
         for v in centerStress.values:
             file.write( "Element nr. %s   Sxx(S11): %s   Syy(S22): %s   Szz(S33): %s    Sxy(S12): %s   Sxz(S13): %s   Syz(S23): %s" %\
+                        (v.elementLabel, v.data[0], v.data[1], v.data[2], v.data[3], v.data[4], v.data[5]) + '\n' )
+        
+        #petla wyswietlajaca wartosc odksztalcen w elementach( wartosci w
+        #jednostkach wymiarowych
+        for v in centerStress.values:
+            file.write( "Element nr. %s   Exx(S11): %s   Eyy(S22): %s   Ezz(S33): %s    Exy(S12): %s   Exz(S13): %s   Eyz(S23): %s" %\
                         (v.elementLabel, v.data[0], v.data[1], v.data[2], v.data[3], v.data[4], v.data[5]) + '\n' )  
 
 
